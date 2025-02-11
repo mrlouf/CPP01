aac8b9fbe2a17dc668f361f527a965a1677abee3
nponchon <nponchon@42.com>
2025-02-11 15:56:19 +0100

    ex01 done?

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,11 +6,40 @@
 /*   By: nponchon <nponchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 15:11:22 by nponchon          #+#    #+#             */
-/*   Updated: 2025/02/11 15:12:06 by nponchon         ###   ########.fr       */
+/*   Updated: 2025/02/11 15:43:51 by nponchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+#include <sstream>
 #include "Zombie.hpp"
 
-int	main()
-{}
+Zombie* zombieHorde( int N, std::string name );
+
+int	main(int ac, char **av)
+{
+
+	if (ac != 3)
+	{
+		std::cout << "Usage: ./MoarBrainz! <Number of Zombies> <Name>" << std::endl;
+		return (1);
+	}
+	
+    std::istringstream iss(av[1]);
+    int N;
+    if (!(iss >> N))
+    {
+        std::cerr << "Invalid number: " << av[1] << std::endl;
+        return 1;
+    }
+	std::string name = av[2];
+
+	Zombie *horde = zombieHorde(N, name);
+
+	horde[1].announce();
+
+	delete[] horde;
+
+	return (0);
+}
--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,7 +6,18 @@
 /*   By: nponchon <nponchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 15:11:40 by nponchon          #+#    #+#             */
-/*   Updated: 2025/02/11 15:11:41 by nponchon         ###   ########.fr       */
+/*   Updated: 2025/02/11 15:55:14 by nponchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+Zombie* zombieHorde(int N, std::string name) {
+
+    Zombie* horde = new Zombie[N];
+	
+    for (int i = 0; i < N; ++i) {
+        horde[i] = Zombie(name);
+    }
+    return horde;
+}
