428c26df5575bf87ee3d85ec4d5af744081c00df
nponchon <nponchon@42.com>
2025-02-11 15:13:01 +0100

    ex01 on the way

---
+++ ex01/Zombie.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.cpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nponchon <nponchon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 13:16:55 by nponchon          #+#    #+#             */
+/*   Updated: 2025/02/11 15:01:52 by nponchon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Zombie.hpp"
+
+// Default Constructor
+Zombie::Zombie() {}
+
+// Constructor with name, calling announce()
+Zombie::Zombie(std::string str) : name(str) {
+	;
+}
+
+// Default Destructor printing message for debug (ie. knowing the time of destruction)
+Zombie::~Zombie() {
+	std::cout << name << " has gone to Zombie's paradise" << std::endl;
+}
+
+// Prints Zombie's name and the announcement
+void	Zombie::announce( void ) {
+	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
---
+++ ex01/Zombie.hpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nponchon <nponchon@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/11 13:16:40 by nponchon          #+#    #+#             */
+/*   Updated: 2025/02/11 14:49:11 by nponchon         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef ZOMBIE_HPP
+# define ZOMBIE_HPP
+
+#include <string>
+#include <iostream>
+
+class Zombie {
+	private:
+		std::string name;
+
+	public:
+		Zombie();
+		Zombie( std::string str );
+		~Zombie();
+		void announce( void );
+};
+
+#endif
