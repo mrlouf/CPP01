4e90225d693e6cffc3b767835f45ebf18bc4736f
nponchon <nponchon@student.42.fr>
2025-02-12 09:41:32 +0100

    modified ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nponchon <nponchon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/11 13:47:32 by nponchon          #+#    #+#             */
-/*   Updated: 2025/02/11 13:57:47 by nponchon         ###   ########.fr       */
+/*   Updated: 2025/02/12 09:37:25 by nponchon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ std::string *stringPTR = &string;
 std::string &stringREF = string;
 
 std::cout << &string << std::endl;
-std::cout << &stringPTR << std::endl;
+std::cout << stringPTR << std::endl;
 std::cout << &stringREF << std::endl;
 
 std::cout << string << std::endl;
